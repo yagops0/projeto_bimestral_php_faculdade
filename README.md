e795fcbbbc13f47f9c5eacd8dfd16458d886e7b9
yagops0 <yagosalvador34@gmail.com>
2024-09-23 20:32:12 -0300

    Criando o html

---
+++ README.md
@@ -0,0 +1,1 @@
+# projeto_bimestral_php_faculdade
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB DESIGN EM FOCO</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="titulo">
+        <h3><center>BEM VINDO AO SITE WEBDESIGN EM FOCO</center></h3>
+    </div>
+
+    <div class="conteudo">
+        <h4>Escolha o idioma para entrar</h4>
+        <nav>
+            <ul>
+                <li><img src="img\bandeira-brasil.jpg" alt=""></li>
+                <li><img src="img\bandeira-espanha.jpg" alt=""></li>
+                <li><img src="img\bandeira-uk.jpg" alt=""></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
